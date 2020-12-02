e64b04d74fb432608228800e1738cc9388467a5c
Boris Mitioglov <boris_mitioglov@epam.com>
2020-12-01 19:13:23 -0800

    configuring db

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -4,6 +4,10 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'edit_blogpost':
+            return state.map((blogPost) => {
+                return blogPost.id === action.payload.id ?  action.payload : blogPost;
+            });
         case 'delete_blogpost':
             return state.filter((blogPost) => blogPost.id !== action.payload);
         case 'add_blogpost':
@@ -23,7 +27,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = dispatch => {
     return (title, content, callback) => {
         dispatch({ type: 'add_blogpost', payload: {title: title, content: content}});
-        callback();
+        if (callback) {
+            callback();
+        }
     };
 };
 
@@ -33,7 +39,18 @@ const deleteBlogPost = dispatch => {
     };
 };
 
+const editBlogPost = dispatch => {
+    return (id, title, content, callback) => {
+        dispatch({
+            type: 'edit_blogpost',
+            payload: {id: id, title: title, content: content}});
+        if (callback) {
+            callback();
+        }
+    };
+};
+
 export const { Context, Provider } = createDataContext(
     blogReducer,
-    { addBlogPost, deleteBlogPost },
-    []);
+    { addBlogPost, deleteBlogPost, editBlogPost },
+    [{title: 'TEST POST', content: 'TEST CONTENT', id: 1}]);
